d07acd28136564a469ed5ff9f6145d5f4ffe4451
Luciano Costa <luccostajr@TRANTOR-WORK>
2023-08-15 10:02:23 -0300

    feat: :sparkles: refatora para permitir que o Indicator Id tenha seu tipo parametrizado, para uso no Soma (IndicatorService) como Long e, futuramente, como UUID

--- src/main/java/com/example/services/core/service/HistoricSymbolSenderService.java
+++ src/main/java/com/example/services/core/service/HistoricSymbolSenderService.java
@@ -5,29 +5,6 @@ import com.example.domain.entities.data.DoubleHistoricData;
 import com.example.services.questdb.service.HistoricSymbolQuestDBService;
 import com.example.services.questdb.service.QuestDBService;
 
-// public class HistoricSymbolSenderService extends SenderService<DefaultHistoricData<UUID>> {
-//   // HistoricData extends DefaultHistoricData<UUID>
-//   // CTOR: SenderService(Entity<T> entity) ==> SenderService(Entity<HistoricData> entity) ==> SenderService(Entity<DefaultHistoricData<UUID>> entity)
-//   // Historic extends AbstractHistoric<UUID>
-//   // abstract class AbstractHistoric<T> extends Entity<DefaultHistoricData<T>>
-//   // Historic extends AbstractHistoric<UUID> ==> Historic extends Entity<DefaultHistoricData<UUID>>
-//   public HistoricSymbolSenderService(Historic historic) { 
-//     super(historic);
-//   } 
-
-//   public void execute() {
-//     HistoricSymbolQuestDBService questDBService = (HistoricSymbolQuestDBService) getQuestDBService(); 
-    
-//     questDBService.processHistoricList(getEntity());
-//     questDBService.flushAndClose();
-//   }
-
-//   @Override
-//   protected QuestDBService createQuestDBService() {
-//     return new HistoricSymbolQuestDBService(); 
-//   }
-// }
-
 public class HistoricSymbolSenderService<T> extends SenderService<DoubleHistoricData<T>> {
   public HistoricSymbolSenderService(Entity<DoubleHistoricData<T>> entity) { 
     super(entity);
